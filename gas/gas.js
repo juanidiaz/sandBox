22bb5e39006d4471517d4507ec972824604310e0
Juan I Diaz <jdi@idiaz.ca>
2018-12-19 14:46:55 -0500

    GasApp working

--- gas/gas.js
+++ gas/gas.js
@@ -10,6 +10,8 @@ var mxn2cad = 0;
 var mxn2usd = 0;
 var usd2cad = 0;
 var usd2mxn = 0;
+const ltr2gal = 0.264172;
+const gal2ltr = 1 / ltr2gal;
 
 //      ARRAYS
 var indexes = ['CAD', 'MXN', 'USD'];
@@ -26,72 +28,129 @@ var currencyURL = "https://currency-exchange.p.rapidapi.com/exchange?q=1.0&from=
 $(document).ready(function () {
 
     const rates = $("#rates");
+    const convert = $("#convert");
 
-    function updateScreen() {
-        for (var i in indexes) {
-            switch (i) {
-                case '0':
-                    var newIndex1 = $("<div>").attr('data-currency', 'cad2mxn').html('<b>$1 ' + indexes[i] + '</b> = $' + cad2mxn + ' ' + indexes[1]).appendTo(rates);
-                    var newIndex2 = $("<div>").attr('data-currency', 'cad2usd').html('<b>$1 ' + indexes[i] + '</b> = $' + cad2usd + ' ' + indexes[2]).appendTo(rates);
-                    break;
+    $('#update').on('click', function () {
+        getIndexes();
+    })
 
-                case '1':
-                    var newIndex1 = $("<div>").attr('data-currency', 'cad2mxn').html('<b>$1 ' + indexes[i] + '</b> = $' + mxn2cad + ' ' + indexes[0]).appendTo(rates);
-                    var newIndex2 = $("<div>").attr('data-currency', 'cad2usd').html('<b>$1 ' + indexes[i] + '</b> = $' + mxn2usd + ' ' + indexes[2]).appendTo(rates);
-                    break;
+    $('.price').on('input', function () {
+        var value = parseFloat(this.value);
+        console.log(value);
+        var priceType = this.id;
+        console.log(this.id);
+        var calculation = 0;
+        switch (priceType) {
+            case 'cadpl':
+                $('#cadpl').css('color', 'red');
+                $('#mxnpl').css('color', 'black').val((value * cad2mxn).toFixed(2));
+                $('#usdpg').css('color', 'black').val(((value * cad2usd) / ltr2gal).toFixed(2));
+                break;
 
-                case '2':
-                    var newIndex1 = $("<div>").attr('data-currency', 'cad2mxn').html('<b>$1 ' + indexes[i] + '</b> = $' + usd2cad + ' ' + indexes[0]).appendTo(rates);
-                    var newIndex2 = $("<div>").attr('data-currency', 'cad2usd').html('<b>$1 ' + indexes[i] + '</b> = $' + usd2mxn + ' ' + indexes[1]).appendTo(rates);
-                    break;
-            }
+            case 'mxnpl':
+                $('#cadpl').css('color', 'black').val((value * mxn2cad).toFixed(2));
+                $('#mxnpl').css('color', 'red');
+                $('#usdpg').css('color', 'black').val(((value * mxn2usd) / ltr2gal).toFixed(2));
+                break;
+
+            case 'usdpg':
+                $('#cadpl').css('color', 'black').val(((value * usd2cad) / gal2ltr).toFixed(2));
+                $('#mxnpl').css('color', 'black').val(((value * usd2mxn) / gal2ltr).toFixed(2));
+                $('#usdpg').css('color', 'red');
+                break;
         }
-    }
+    })
 
-    function getIndex(from, to, saveTo) {
+    function getIndexes() {
 
+        rates.empty();
+
+        var dt = new Date();
+        rates.append('<b>Last update: </b>' + dt);
+
+        // Get CAD2MXN
         $.ajax({
-            url: currencyURL + from + "&to=" + to,
+            url: currencyURL + indexes[0] + "&to=" + indexes[1],
             headers: {
                 "X-RapidAPI-Key": RapidAPI
             },
             method: "GET"
         }).then(function (response) {
-            console.log(currencyURL + from + "&to=" + to);
             index = parseFloat(response).toFixed(4);
-            console.log('Value of index: ' + index);
+            console.log("cad2mxn: " + index);
+            cad2mxn = index;
+            var newIndex1 = $("<div>").attr('data-currency', 'cad2mxn').html('<b>$1 ' + indexes[0] + '</b> = $' + cad2mxn + ' ' + indexes[1]).appendTo(rates);
         });
-        var xx = saveTo;
-            // console.log('Value of index: ' + index);
+
+        // Get CAD2USD
+        $.ajax({
+            url: currencyURL + indexes[0] + "&to=" + indexes[2],
+            headers: {
+                "X-RapidAPI-Key": RapidAPI
+            },
+            method: "GET"
+        }).then(function (response) {
+            index = parseFloat(response).toFixed(4);
+            console.log("cad2usd: " + index);
+            cad2usd = index;
+            var newIndex2 = $("<div>").attr('data-currency', 'cad2usd').html('<b>$1 ' + indexes[0] + '</b> = $' + cad2usd + ' ' + indexes[2]).appendTo(rates);
+        });
+
+        // Get MXN2CAD
+        $.ajax({
+            url: currencyURL + indexes[1] + "&to=" + indexes[0],
+            headers: {
+                "X-RapidAPI-Key": RapidAPI
+            },
+            method: "GET"
+        }).then(function (response) {
+            index = parseFloat(response).toFixed(4);
+            console.log("mxn2cad: " + index);
+            mxn2cad = index;
+            var newIndex3 = $("<div>").attr('data-currency', 'cad2mxn').html('<b>$1 ' + indexes[1] + '</b> = $' + mxn2cad + ' ' + indexes[0]).appendTo(rates);
+        });
+
+        // Get MXN2USD
+        $.ajax({
+            url: currencyURL + indexes[1] + "&to=" + indexes[2],
+            headers: {
+                "X-RapidAPI-Key": RapidAPI
+            },
+            method: "GET"
+        }).then(function (response) {
+            index = parseFloat(response).toFixed(4);
+            console.log("mxn2usd: " + index);
+            mxn2usd = index;
+            var newIndex4 = $("<div>").attr('data-currency', 'cad2usd').html('<b>$1 ' + indexes[1] + '</b> = $' + mxn2usd + ' ' + indexes[2]).appendTo(rates);
+        });
+
+        // Get USD2CAD
+        $.ajax({
+            url: currencyURL + indexes[2] + "&to=" + indexes[0],
+            headers: {
+                "X-RapidAPI-Key": RapidAPI
+            },
+            method: "GET"
+        }).then(function (response) {
+            index = parseFloat(response).toFixed(4);
+            console.log("usd2cad: " + index);
+            usd2cad = index;
+            var newIndex5 = $("<div>").attr('data-currency', 'cad2mxn').html('<b>$1 ' + indexes[2] + '</b> = $' + usd2cad + ' ' + indexes[0]).appendTo(rates);
+        });
+
+        // Get USD2MXN
+        $.ajax({
+            url: currencyURL + indexes[2] + "&to=" + indexes[1],
+            headers: {
+                "X-RapidAPI-Key": RapidAPI
+            },
+            method: "GET"
+        }).then(function (response) {
+            index = parseFloat(response).toFixed(4);
+            console.log("usd2mxn: " + index);
+            usd2mxn = index;
+            var newIndex6 = $("<div>").attr('data-currency', 'cad2usd').html('<b>$1 ' + indexes[2] + '</b> = $' + usd2mxn + ' ' + indexes[1]).appendTo(rates);
+        });
     }
-
-    // getIndex(indexes[0], indexes[1], cad2mxn);
-    // getIndex(indexes[0], indexes[2], cad2usd);
-    // getIndex(indexes[1], indexes[0], mxn2cad);
-    // getIndex(indexes[1], indexes[2], mxn2usd);
-    // getIndex(indexes[2], indexes[0], usd2cad);
-    // getIndex(indexes[2], indexes[1], usd2mxn);
-
-    var test = getIndex(indexes[0], indexes[1]);
-    console.log('Value: ' + test);
-
-    updateScreen();
-
-
-    $.ajax({
-        url: currencyURL + indexes[0] + "&to=" + indexes[1],
-        headers: {
-            "X-RapidAPI-Key": RapidAPI
-        },
-        method: "GET"
-    }).then(function (response) {
-        console.log(currencyURL + indexes[0] + "&to=" + indexes[1]);
-        var index = parseFloat(response).toFixed(4);
-        cad2mxn = index;
-        console.log(index);
-    });
-
-
-
-
+    getIndexes();
 });
